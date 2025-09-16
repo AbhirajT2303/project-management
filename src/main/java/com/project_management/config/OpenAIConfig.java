6e90854bff551d4c6afb2266d116f8c96e866ebd
Abhishek <abhishektalkokul@gmail.com>
2025-09-16 15:58:16 +0530

    deploy: railways chamges for config

--- src/main/java/com/project_management/config/OpenAIConfig.java
+++ src/main/java/com/project_management/config/OpenAIConfig.java
@@ -1,20 +1,23 @@
 package com.project_management.config;
 
 //import com.theokanning.openai.OpenAiService;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import com.theokanning.openai.service.OpenAiService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 public class OpenAIConfig {
+    @Value("${openai.api.key}")
+    String apiKey;
 
     @Bean
     public OpenAiService openAiService() {
-        String apiKey = System.getenv("OPENAI_API_KEY");
+
         if (apiKey == null) {
-            apiKey = "api_key"; 
+            apiKey = "api_key";
         }
         return new OpenAiService(apiKey);
     }
